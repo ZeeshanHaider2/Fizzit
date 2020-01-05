d71571777958a130f267afb37db7a3d313271a6b
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2020-01-05 20:10:48 +0100

    dev.js files removed from git ignore

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+//Development keys are stored here!
+module.exports = {
+  googleClientID:
+    "951400120480-0u031rcpot3i4je4c9iaain2snmvqdvt.apps.googleusercontent.com",
+  googleClientSecret: "OWEqm5WPgc_iUkgo8Fwal2xU",
+  mongoURI:
+    "mongodb+srv://Zeeshan:Coei3kNy@fizzit-zfyn2.mongodb.net/users?retryWrites=true&w=majority",
+  cookieKey: "hbvdhbvdhbhdbhdbhdbcbjjcbwjbckwckw",
+  stripePublishableKey: "pk_test_hO2e8Zp6PrKDlLS6MjH7rzQk00L58TEbKQ",
+  stripeSecretKey: "sk_test_kHDsxlDzXsgiGZ7SaKtqdvkn001xD47RjS"
+};
