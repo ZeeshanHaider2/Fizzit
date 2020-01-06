b71e01331d007b78e25ab2f9b5b1372fcfc98e4e
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2020-01-06 19:56:48 +0100

    change to development mode

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+module.exports = require("./dev");
+/*
 if (process.env.NODE_ENV === "production") {
   //We are in production-return the prod keys!
   module.exports = require("./prod");
@@ -5,3 +7,4 @@ if (process.env.NODE_ENV === "production") {
   //We are in development-return the dev keys!
   module.exports = require("./dev");
 }
+*/
