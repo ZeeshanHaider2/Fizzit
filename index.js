efad19cf12c87a744459947796845c9eff196970
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2020-01-06 20:04:03 +0100

    comment production

--- index.js
+++ index.js
@@ -24,18 +24,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app); //require will immediatelly will turn into a function which we will call by app object
 
-if (process.env.NODE_ENV === "production") {
-  //express will serve up production assets
-  //like our main.js file, or main.css file
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   //express will serve up production assets
+//   //like our main.js file, or main.css file
+//   app.use(express.static("client/build"));
 
-  //Express will serve up the index.html file
-  //if it doesn't recognize the route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   //Express will serve up the index.html file
+//   //if it doesn't recognize the route
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000; //5000, if running server locally
 app.listen(PORT);
